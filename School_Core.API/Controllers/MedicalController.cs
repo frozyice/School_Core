8aa0f5674dd976e4089d34a77969cb56906395af
Karmo Kiima <karmo.kiima@agileworks.eu>
2020-11-03 17:30:02 +0200

    Convert MedicalController to async

--- School_Core.API/Controllers/MedicalController.cs
+++ School_Core.API/Controllers/MedicalController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using School_Core.API.Contexts;
 using School_Core.API.DTOs;
 using School_Core.API.Models;
@@ -20,46 +22,46 @@ namespace School_Core.API.Controllers
         }
         
         [HttpGet]
-        public IActionResult GetMedicals()
+        public async Task<IActionResult> GetMedicals()
         {
-            var medicals = _dbContext.Medicals.ToList();
+            var medicals = await _dbContext.Medicals.ToListAsync();
             var medicalsGetDtos = new List<MedicalGetDto>();
             foreach (var medical in medicals)
             {
-                var sickLeaveGetDto = new MedicalGetDto()
+                var medicalGetDto = new MedicalGetDto()
                 {
                     Id = medical.Id,
                     StudentId = medical.StudentId, 
                     Active = medical.DateTo is null ? "Yes" : "No",
                     Reason = medical.Reason
                 };
-                medicalsGetDtos.Add(sickLeaveGetDto);
+                medicalsGetDtos.Add(medicalGetDto);
             }
             return Ok(medicalsGetDtos);
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetMedical(Guid id)
+        public async Task<IActionResult> GetMedical(Guid id)
         {
-            var medical = _dbContext.Medicals.Find(id);
+            var medical = await _dbContext.Medicals.FindAsync(id);
             if (medical is null)
             {
                 return NotFound();
             }
-            var sickLeaveGetDto = new MedicalGetDto()
+            var medicalGetDto = new MedicalGetDto()
             {
                 Id = medical.Id,
                 StudentId = medical.StudentId, 
                 Active = medical.DateTo is null ? "Yes" : "No",
                 Reason = medical.Reason
             };
-            return Ok(sickLeaveGetDto);
+            return Ok(medicalGetDto);
         }
         
         [HttpGet("student/{studentId}")]
-        public IActionResult GetStudentMedicals(Guid studentId)
+        public async Task<IActionResult> GetStudentMedicals(Guid studentId)
         {
-            var medicals = _dbContext.Medicals.Where(x=>x.StudentId == studentId).ToList();
+            var medicals = await _dbContext.Medicals.Where(x=>x.StudentId == studentId).ToListAsync();
             var medicalsGetDtos = new List<MedicalGetDto>();
             foreach (var medical in medicals)
             {
@@ -76,39 +78,39 @@ namespace School_Core.API.Controllers
         }
         
         [HttpPost("student/{studentId}")]
-        public IActionResult AddMedical(Guid studentId, MedicalPostDto medicalPostDto)
+        public async Task<IActionResult> AddMedical(Guid studentId, MedicalPostDto medicalPostDto)
         {
             var medical = new Medical(studentId, medicalPostDto.Reason);
-            _dbContext.Add(medical);
-            _dbContext.SaveChanges();
+            await _dbContext.AddAsync(medical);
+            await _dbContext.SaveChangesAsync();
             return CreatedAtAction(nameof(GetMedical), new {id = medical.Id}, medicalPostDto);
         }
         
         [HttpPut("{id}")]
-        public IActionResult UpdateMedical(Guid id, MedicalPutDto medicalPutDto)
+        public async Task<IActionResult> UpdateMedical(Guid id, MedicalPutDto medicalPutDto)
         {
-            var medical = _dbContext.Medicals.FirstOrDefault(x => x.Id == id);
+            var medical = await _dbContext.Medicals.FirstOrDefaultAsync(x => x.Id == id);
             if (medical == null)
             {
                 return NotFound();
             }
 
             medical.ChangeReason(medicalPutDto.Reason);
-            _dbContext.SaveChanges();
+            await  _dbContext.SaveChangesAsync();
             return NoContent();
         }
         
         [HttpDelete("{id}")]
-        public IActionResult CloseMedical(Guid id)
+        public async Task<IActionResult> CloseMedical(Guid id)
         {
-            var medical = _dbContext.Medicals.Find(id);
+            var medical = await _dbContext.Medicals.FindAsync(id);
             if (medical is null)
             {
                 return BadRequest();
             }
 
             medical.Close();
-            _dbContext.SaveChanges();
+            await _dbContext.SaveChangesAsync();
             return NoContent();
         }
 
