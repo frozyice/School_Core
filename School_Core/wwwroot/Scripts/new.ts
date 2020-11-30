064390df2c4d45ce5e0c6ef5978c61a767e1b93c
Karmo Kiima <karmo.kiima@agileworks.eu>
2020-11-30 14:04:06 +0200

    Add TypeScript

---
+++ School_Core/wwwroot/Scripts/new.ts
@@ -0,0 +1,18 @@
+﻿
+function reverse() : void {
+    let content: string = (<HTMLInputElement>document.getElementById("inputSentence")).value.toLowerCase()
+    let ContentCollection: string[] = content.split(" ")
+    ContentCollection.reverse()
+    
+    let reversedContent = ContentCollection.toString()
+    let newContent = reversedContent.replace(/[,]/g, " ")
+    
+    document.getElementById("outputSentence").innerHTML = newContent
+    
+}
+
+document.getElementById("inputSentence").addEventListener("keyup", reverse)
+
+window.onload = function (){
+    reverse()
+}
